c1f926599db219cd02dbf54e0744641dff6b344d
shipp11 <shippdaddy1123@gmail.com>
2017-12-09 10:57:58 -0500

    family

---
+++ family.rb
@@ -0,0 +1,177 @@
+def people ()
+	josh = {:name => "josh", :age => 32, :kids => "yes", :married => "no"}
+	sean = {:name => "sean", :age => 26, :kids => "yes", :married => "no"}
+	erika = {:name => "erika", :age => 24, :kids => "yes", :married => "no"}
+	taylor = {:name => "taylor", :age => 20, :kids => "no", :married => "no"}
+	bobby = {:name => "bobby", :age => 30, :kids => "yes", :married => "yes"}
+
+if sean[:age] == taylor[:age]
+	puts "age = true"
+end
+
+if sean[:kids] == taylor[:kids]
+	puts "kids = true"
+end
+
+if sean[:married] == taylor[:married]
+	puts "married = true"
+end
+
+if erika[:age] == sean[:age]
+	puts "age = true"
+end
+
+if erika[:kids] == sean[:kids]
+	puts "kids = true"
+end
+
+if erika[:married] == sean[:married]
+	puts "married = true"
+end
+
+if erika[:age] == taylor[:age]
+	puts "age = true"
+end
+
+if erika[:kids] == taylor[:kids]
+	puts "kids = true"
+end
+
+if erika[:married] == taylor[:married]
+	puts "married = true"
+end
+
+if josh[:age] == sean[:age]
+	puts "age = true"
+end
+
+if josh[:kids] == sean[:kids]
+	puts "kids = true"
+end
+
+if josh[:married] == sean[:married]
+	puts "married = true"
+end
+
+if josh[:age] == erika[:age]
+	puts "age = true"
+end
+
+if josh[:kids] == erika[:kids]
+	puts "kids = true"
+end
+
+if josh[:married] == erika[:married]
+	puts "married = true"
+end
+
+if josh[:age] == taylor[:age]
+	puts "age = tnrue"
+end
+
+if josh[:kids] == taylor[:kids]
+	puts "kids = true"
+end
+
+if josh[:married] == taylor[:married]
+	puts "married = true"
+end
+
+if bobby[:age] == sean[:age]
+	puts "age = true"
+end
+
+if bobby[:kids] == sean[:kids]
+	puts "kids = true"
+end
+
+if bobby[:married] == sean[:married]
+	puts "married = true"
+end
+
+if bobby[:age] == erika[:age]
+	puts "age = true"
+end
+
+if bobby[:kids] == erika[:kids]
+	puts "kids = true"
+end
+
+if bobby[:married] == erika[:married]
+	puts "married = true"
+end
+
+if bobby[:age] == taylor[:age]
+	puts "age = true"
+end
+
+if bobby[:kids] == taylor[:kids]
+	puts "kids = true"
+end
+
+if bobby[:married] == taylor[:married]
+	puts "married = true"
+end
+
+if bobby[:age] == josh[:age]
+	puts "age = true"
+end
+
+if bobby[:kids] == josh[:kids]
+	puts "kids = true"
+end
+
+if bobby[:married] == josh[:married]
+	puts "married = true"
+end
+
+if taylor[:age] == sean[:age]
+	puts "age = true"
+end
+
+if taylor[:kids] == sean[:kids]
+	puts "kids = true"
+end
+
+if taylor[:married] == sean[:married]
+	puts "married = true"
+end
+
+if taylor[:age] == josh[:age]
+	puts "age = true"
+end
+
+if taylor[:kids] == josh[:kids]
+	puts "kids = true"
+end
+
+if taylor[:married] == josh[:married]
+	puts "married = true"
+end
+
+if taylor[:age] == erika[:age]
+	puts "age = true"
+end
+
+if taylor[:kids] == erika[:kids]
+	puts "kids = true"
+end
+
+if taylor[:married] == erika[:married]
+	puts "married = true"
+end
+
+if taylor[:age] == bobby[:age]
+	puts "age = true"
+end
+
+if taylor[:kids] == bobby[:kids]
+	puts "kids = true"
+end
+
+if taylor[:married] == bobby[:married]
+	puts "married = true"
+end
+end
+
+people ()
